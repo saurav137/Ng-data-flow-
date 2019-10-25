31f4ef4ec35dc225e0fb4aaad3b9160a159e72db
sauravsinha137 <k123saurav@gmail.com>
2019-10-25 10:39:49 +0530

    added child to parent component interaction

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent implements OnInit {
-   @Input()
-  uname:string;
-  ngOnInit() {
-  }
+export class TestComponent  {
+  @Output()
+  notify:EventEmitter<string> = new EventEmitter<string>();
+
+   passData(){
+     this.notify.emit("this is coming from child component")
+   }
  
 }
